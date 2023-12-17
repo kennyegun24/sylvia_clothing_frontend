b7ef4a486306846978f9fb7176f064ff324a5e76
Kenny Elias <kennyegun240@gmail.com>
2023-12-17 03:57:32 +0100

    Created and styled cart component Implemented hide and open cart  func

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
+import CartModal from "./components/cart/CartModal";
 import Footer from "./components/footer/Footer";
 import Nav from "./components/nav/Nav";
+import { ShowCartProvider } from "./hooks/showCart";
 
 function App() {
   return (
     <div className="app flex column">
-      <Nav />
-      <div style={{ minHeight: "70vh" }}></div>
-      <div className="footerDiv">
-        <Footer />
-      </div>
+      <ShowCartProvider>
+        <Nav />
+        <CartModal />
+        <div style={{ minHeight: "70vh" }}></div>
+        <div className="footerDiv">
+          <Footer />
+        </div>
+      </ShowCartProvider>
     </div>
   );
 }
