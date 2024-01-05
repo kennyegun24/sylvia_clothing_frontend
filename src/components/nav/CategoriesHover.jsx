589518038b875dc180a2e23342e21e2fc71bca1b
Kenny Elias <kennyegun240@gmail.com>
2024-01-05 23:10:08 +0100

    Make categories display on click

---
+++ src/components/nav/CategoriesHover.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CategoriesHover = () => {
+  return (
+    <div className="collections_hover_div flex column gap1rem">
+      <h2 className="font20">Collections</h2>
+      <hr />
+      <div className="flex wrap gap1rem">
+        <a href="/collections">All Collections</a>
+        <a href="/">Best Rated</a>
+        <a href="/">New Arrivals</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+        <a href="/">I AM KENNY</a>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesHover;
